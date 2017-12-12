bad7671a4eee62a1b95fce98e54616f8d16839df
Ratnakar <asara.ratnakar@gmail.com>
2017-12-12 15:20:06 -0500

    Add files via upload

---
+++ channel2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#kill -9 $(ps -ef | grep deliver_stdout | grep -v grep | awk '{print $2}')
+
+## remove old logs
+printf "\n Cleanup the logs"
+rm -rf channel2-log*.txt
+
+C_LIMIT=100
+CLIENTS=${1:-$C_LIMIT}
+printf "\n Total Client requested : $CLIENTS\n"
+for ((c=0;c<$CLIENTS;c++))
+do
+ 	# printf "\n--------- START Index: $c\n"
+ 	./deliver_stdout --channelID mychannel1 --quiet --seek -2 >& channel2-log$c.txt &
+	 printf ""
+ 	# printf "\n--------- END Index: $c\n"
+done
+printf "\n\n Wait for 20 secs ..."
+sleep 20
+
+counter=0
+for ((c=0;c<$CLIENTS;c++))
+do
+	lineCount=$(cat channel2-log$c.txt | wc -l)
+	if [ $lineCount -ne 2 ]; then
+		printf "\n #### Client $c failed to fetch all the blocks, could fetch only $lineCount blocks\n"
+		counter=` expr $counter + 1 `
+	fi 
+done
+
+if [ $counter -ne 0 ]; then
+	printf "\n ###### $counter Clients failed to fetch all the blocks ####### \n"
+	exit 0
+fi
+
+echo
+echo "===================== All GOOD, execution completed ===================== "
+echo
+
+exit 0
